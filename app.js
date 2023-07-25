0309e94a99d92c619e23a8666eba06333cfd2f79
Matthew Postrel <matthewpostrel@gmail.com>
2023-07-25 16:49:06 -0400

    added local storage completion

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+const colorPicker = Array(itemsArray.length).fill(0);
+const colorArray = localStorage.getItem('colors') ? JSON.parse(localStorage.getItem('colors')) : [];
 const item  = document.querySelector('.input-text')
 const errContainer = document.getElementById("err-container");
 
-console.log(itemsArray);
 
 function display () {
     let html = '';
-    for(let i =0; i < itemsArray.length; i++){
-        html += `
-        <div class="items">
-            <div class="task">${itemsArray[i]}</div>
-            <i class="fa-solid fa-trash trash"></i>
-        </div>
-        `;
+    for(let i = 0; i < itemsArray.length; i++){
+            html += `
+            <div class="items" >
+                <div class="task">${itemsArray[i]}</div>
+                <i class="fa-solid fa-trash trash"></i>
+            </div>
+            `;
     }
     document.querySelector('.to-do-list').innerHTML = html;
     deleteItem();
+    completed();
 }
 
 document.querySelector('.button').addEventListener('click', (e) => {
     e.preventDefault();
     if (!item.value) {
-        console.log('must fill space');
         error();
     } else {
         const capital = makeCapital(item.value);
         createItem(capital);
     }
-    
 })
 
 function deleteItem() {
@@ -43,6 +43,7 @@ function deleteItem() {
 
 function createItem (item) {
     itemsArray.push(item);
+    completed(item);
     localStorage.setItem('items', JSON.stringify(itemsArray));
     item= "";
     location.reload();
@@ -69,5 +70,37 @@ function error () {
 }
 
 
+function completed() {
+    const completed = document.querySelectorAll('.items');
+    completed.forEach( (el, i) => {
+        el.addEventListener('click', () => {
+            el.classList.toggle('green');
+
+            if (colorPicker[i] == 0){
+                colorPicker[i] = 1;
+            } else {
+                colorPicker[i] = 0;
+            }
+            localStorage.setItem('colors', JSON.stringify(colorPicker));
+        })
+    })
+}
+
+function dynamicColor () {
+    const completed = document.querySelectorAll('.items');
+    for(let i = 0; i < completed.length; i++){
+        if(colorArray[i] === 1) {
+            completed[i].classList.add('green');
+            if (colorPicker[i] == 0){
+                colorPicker[i] = 1;
+            } else {
+                colorPicker[i] = 0;
+            }
+            localStorage.setItem('colors', JSON.stringify(colorPicker));
+        }
+    }
+}
+
 item.focus();
 display();
+dynamicColor();
